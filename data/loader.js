4d8991ec0204d786bb358da996e8e9597233a8f7
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-03-31 14:47:59 -0700

    warning, button, etc

--- data/loader.js
+++ data/loader.js
@@ -1,13 +1,24 @@
-self.port.on('injectBot', function(botCode){
-    var script = document.getElementById('#robot-code')
-    if (script) {
-        window.setTimeout(function() {
+self.port.on('injectWarning', function (){
+    var btnBar = $('.buttons-bar')
+    btnBar.append($('<p>').text('Please set your bot in FightCode Utils ' +
+                                'preferneces'))
+})
+
+self.port.on('injectButton', function(){
+    var script = $('#robot-code')
+    if (script.length) {
         var btnBar = $('.buttons-bar')
-        var btn = $('<a>')
+        var btn = $('<button>')
         btnBar.append(btn)
-        btn.addClass('btn').text('Load Bot').click(function() {
-            $('#code').val(botCode)
-            $('#robo-code-form').submit()
-        })}, 1000)
+        btn.addClass('btn').text('Load Bot').click(function(ev) {
+            ev.preventDefault()
+            ev.stopPropagation()
+            self.port.emit('readyToInject')
+        })
     }
+})
+
+self.port.on('injectBot', function(botCode){
+    $('#code').val(botCode)
+    $('#robo-code-form').submit()
 })
